f0cc4650bc4f6190d243b9a6d8de491419191c82
Daduck <daduck88@gmail.com>
2017-07-27 16:12:59 -0500

    Add other fields on details

--- PopularMoviesApp/app/src/main/java/com/android/popularmoviesapp/app/detail/DetailActivity.java
+++ PopularMoviesApp/app/src/main/java/com/android/popularmoviesapp/app/detail/DetailActivity.java
@@ -2,10 +2,13 @@ package com.android.popularmoviesapp.app.detail;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.popularmoviesapp.R;
+import com.android.popularmoviesapp.app.App;
 import com.android.popularmoviesapp.model.Movie;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by Daduck on 7/27/17.
@@ -14,6 +17,10 @@ import com.android.popularmoviesapp.model.Movie;
 public class DetailActivity  extends AppCompatActivity {
     public static final String MOVIE = "movie";
     private TextView tvTitle;
+    private TextView tvReleaseDate;
+    private TextView tvVoteAverage;
+    private TextView tvPlotSynopsis;
+    private ImageView ivPoster;
     private Movie movie;
 
     @Override
@@ -26,6 +33,12 @@ public class DetailActivity  extends AppCompatActivity {
 
     private void initViews() {
         tvTitle = (TextView) findViewById(R.id.tv_movie_detail_title);
+        tvReleaseDate = (TextView) findViewById(R.id.tv_movie_detail_release_day);
+        tvVoteAverage = (TextView) findViewById(R.id.tv_movie_detail_vote_average);
+        tvPlotSynopsis = (TextView) findViewById(R.id.tv_movie_detail_plot);
+        ivPoster = (ImageView) findViewById(R.id.iv_movie_detail_poster);
+
+//        release date, movie poster, vote average, and plot synopsis.
     }
 
     private void initData() {
@@ -33,5 +46,11 @@ public class DetailActivity  extends AppCompatActivity {
             movie = getIntent().getParcelableExtra(MOVIE);
         }
         tvTitle.setText(movie.getTitle());
+        tvReleaseDate.setText(movie.getReleaseDate());
+        tvVoteAverage.setText(movie.getVoteAverage() + "");
+        tvPlotSynopsis.setText(movie.getOverview());
+
+        String path = App.context.getString(R.string.url_image_thumb_path) + movie.getPosterPath();
+        Picasso.with(getBaseContext()).load(path).into(ivPoster);
     }
 }
